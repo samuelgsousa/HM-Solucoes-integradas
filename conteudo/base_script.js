ce8b785fcbe612c6082e81b2578028a4878a82f5
Samuel Sousa <143044276+samuelgsousa@users.noreply.github.com>
2024-02-02 20:43:52 -0300

    02/02/2024
    Aqui foi criado o menu de hamburger que aparecerá em telas menores, de dispositivos mobile, usando Javascript. Por enquanto feito apenas na versão escura do site

--- conteudo/base_script.js
+++ conteudo/base_script.js
@@ -1,5 +1,23 @@
 $(document).ready(() => {
     
+    let redim = () => {
+        if($(window).width() <= 600){
+            $('header').removeClass("header_desktop").addClass("header_mobile")
+            $('nav').removeClass("nav_desktop").addClass("nav_mobile")
+            $('#btn-mobile').css('display', 'flex')
+        } else{
+            $('header').removeClass("header_mobile").addClass("header_desktop")
+            $('nav').removeClass("nav_mobile").addClass("nav_desktop")
+            $('#btn-mobile').css('display', 'none')
+            $('#btn-mobile').removeClass("active")
+            $('nav').removeClass("active")
+            $('#hamburger').removeClass("active")
+        }
+    }
+    redim()
+    
+    window.addEventListener("resize", redim)
+    
     let tema = window.matchMedia('(prefers-color-scheme: dark)').matches ?  $('#base').attr("href", "base_style_dark_mode.css") : $('#base').attr("href", "base_style.css") //essa função detecta se o usuário utiliza o modo escuro, caso positivo o estilo base da página mudará. Se não, ele irá inicidar com o estilo base padrão, apenas para garantir que não ocorram erros
 
     $('#' + $('title').text()).addClass("selectedPage") // essa linha pega o nome da página e localiza o link com o mesmo id para deixar selecionado
@@ -8,8 +26,24 @@ $(document).ready(() => {
     
     $('nav > a').mouseout(() => $('#' + $('title').text()).addClass("selectedPage")) //quando o usuário tirar o mouse do link irá localizar o link cujo o id seja igual o nome da página
     
-    
+    const btnMobile = document.querySelector('button#btn-mobile')
+    btnMobile.addEventListener('click', toggleMenu)
+    btnMobile.addEventListener('touchstart', toggleMenu)
 
+    function toggleMenu(event){
+        if(event.type === 'touchstart') event.preventDefault() /*essa linha serve para previnir o evento click de ocorrer caso o touchstart ocorra, pois o touchstart gera um click com atraso de alguns milisegundos*/
+        const nav = document.querySelector('nav')
+        nav.classList.toggle('active'); //toggle significa adicione caso não tenha, remova caso tenha
+        /*acessibilidade*/
+        const active = nav.classList.contains('active')
+        event.currentTarget.setAttribute('aria-expanded', active)
+        if(active) {event.currentTarget.setAttribute('aria-label', 'Fechar Menu')} else event.currentTarget.setAttribute('aria-label', 'Abrir Menu') /*acessibilidade*/
+
+        const btn = document.querySelector('span#hamburger')
+        btn.classList.toggle('active')
+    }
+
+    
 });
 
 
