65828e5169d6845534df01e951cb066cd5500d8f
Samuel Sousa <143044276+samuelgsousa@users.noreply.github.com>
2024-02-05 02:55:48 -0300

    05/02/2024
    consegui fazer o carrossel responsivo. Ainda é necessário alinhar as imagens corretamente para que não fique excessivamente cortadas

--- conteudo/index_script.js
+++ conteudo/index_script.js
@@ -1,15 +1,32 @@
 let count = 1;
 document.getElementById("radio1").checked = true;
 
-setInterval(function(){
-    nextImage()
-}, 2000)
+//  setInterval(function(){
+//      nextImage()
+//  }, 2000)
 
-function nextImage(){
-    count++
-    if(count > 4){
-        count = 1
-    }
+// function nextImage(){
+//     count++
+//     if(count > 4){
+//         count = 1
+//     }
 
-    document.getElementById("radio" + count).checked = true;
-}
+//     document.getElementById("radio" + count).checked = true;
+// }
+
+window.addEventListener("resize",redimCarrossel)
+
+
+//redimCarrossel()
+
+// function redimCarrossel(){
+//     let windowHeight = window.innerHeight
+//     let cHeight =  windowHeight * 0.5
+//    $('.slider').css("height", `${cHeight}px`)
+
+ 
+// }
+
+
+
+//800px
