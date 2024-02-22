b6a4db3b13ff799233fccc08ff86035c2786f66e
Samuel Sousa <143044276+samuelgsousa@users.noreply.github.com>
2024-02-21 23:20:11 -0300

    21/02/2024
    Alterações:
    -> Carrossel refeito. Mil vezes melhor, mais prático, mais responsivo e mais versátil
    -> Ajustes em Index na parte de descrição, mas ainda precisam ser feitos mais alguns ajustes

--- conteudo/Script/index_script.js
+++ conteudo/Script/index_script.js
@@ -1,52 +1,18 @@
-window.addEventListener("resize",redimCarrossel)
-
-let count = 1;
-let sld = 0;
- //depois mudar a referencia, ao invés de usar radio usar as imagens em si
-
-   setInterval(function(){
-       nextImage()
-   }, 5000)
-
+    const box = document.querySelector(".container")
+    const imagens = document.querySelectorAll(".container img")
+    let cont = 0
+  
  function nextImage(){
-     count++
-     
-     if(count > 4){
-         count = 1
-         sld = -25
-     }
-     //slide first
-   
-     sld += 25
-
-     $('.first').css("margin-left", `-${sld}%`) //a transição é feita diminuindo a margem de 25& em 25%, por conta de serem 4 imagens. Depois tentar criar um carrossel utilizando o create element. Para não ficar muito pesado, você pode utilizar variáveis que pegem as fotos. Ai cria um elemento na esquerda da div, move a foto, e deleta a imagem que sumiu na esquerda
+    cont++
+    if(cont > imagens.length - 1){
+        cont = 0
+    }
+    box.style.transform = `translateX(${-cont * 100}%)`
  }
 
-
-
-redimCarrossel()
-
- function redimCarrossel(){ //essa função irá ajustar as imagens para que não fique estranhas quando o tamanho do carrossel diminuir
-     let windowW = window.innerWidth
-     let disp = document.querySelector(".slider").clientHeight
-        $('#display').css('height', `${disp}px`) //ajustando para a altura da div display ser sempre do mesmo tamanho que o slider
-
-    if(windowW < 2300){ //todas as imagens começam a ficar com margem excessiva com menos de 900px, por isso um único if. O transition é para tornar o ajustes mais suaves caso a pessoa diminua a tela
-        $('#img2').css('margin-top', `${0}px`)
-        $('#img3').css('margin-top', `${0}px`)
-        $('#img4').css('margin-top', `${0}px`)
-    }else {
-        $('#img2').css('transition', '0.5s')
-        $('#img3').css('transition', '0.5s')
-
-        $('#img2').css('margin-top', `${-3}%`)
-        $('#img3').css('margin-top', `${-2}%`)
-        $('#img4').css('margin-top', `${-2}%`)
-      }
- }
 
  
 
-
-//730px
-//550
+  setInterval(function(){
+     nextImage()
+ }, 4500)
