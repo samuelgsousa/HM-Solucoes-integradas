ac7a0eed81268ad6b37e9bbc9343980dcf3bd045
Samuel Sousa <143044276+samuelgsousa@users.noreply.github.com>
2024-02-25 21:09:30 -0300

    25/02/2024
    Alterações:
    -> criado um dos carrosséis da página "serviços prestados", com radio buttons para que o usuário consiga seleciona alguma das imagens

--- conteudo/Script/servicos_script.js
+++ conteudo/Script/servicos_script.js
@@ -9,16 +9,27 @@ const imagens = document.querySelectorAll(".container img")
 let cont = 0
 
 function nextImage(){
+
 cont++
-if(cont > imagens.length - 1){
-    cont = 0
-}
+
+if(cont > imagens.length - 1) cont = 0
+
 box.style.transform = `translateX(${-cont * 100}%)`
+
+$(`#C1R${cont + 1}`).prop("checked", true)
+
 }
 
+$('input[name="radioC1"]').click( () => { 
+    var teste = $('input[name="radioC1"]:checked').val()
+    box.style.transform = `translateX(${(-teste + 1) * 100}%)`
+    cont = teste - 1
+    clearInterval(timer)
+})
 
+ let timer = setInterval(function(){
+   nextImage()
+  }, 4500)
 
-
-// setInterval(function(){
-//  nextImage()
-// }, 4500)
+  //colcoar alguma forma de quando a pessoa clicar na foto, aumentar o tamanho dela e parar o carrossel até ela fechar a imagem
+  //colocar algo para quando o usuário clicar no radio, resetar a contagem do setInterval
